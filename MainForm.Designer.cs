8edb459ea5c0e5893bd8f3f58d89e6f119ac5a93
Daniel <Daniel@Daniel.com>
2021-01-15 17:05:41 +0800

    first

---
+++ MainForm.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace ShowRTF
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.txtInput = new System.Windows.Forms.RichTextBox();
+            this.txtOutput = new System.Windows.Forms.RichTextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 90F));
+            this.tableLayoutPanel1.Controls.Add(this.txtInput, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtOutput, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btnConvert, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnClear, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtInput.Location = new System.Drawing.Point(3, 3);
+            this.txtInput.Name = "txtInput";
+            this.tableLayoutPanel1.SetRowSpan(this.txtInput, 2);
+            this.txtInput.Size = new System.Drawing.Size(704, 74);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.Text = "";
+            this.txtInput.DoubleClick += new System.EventHandler(this.txtInput_DoubleClick);
+            // 
+            // txtOutput
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.txtOutput, 2);
+            this.txtOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtOutput.Location = new System.Drawing.Point(3, 83);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(794, 364);
+            this.txtOutput.TabIndex = 1;
+            this.txtOutput.Text = "";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnConvert.Location = new System.Drawing.Point(713, 3);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(84, 34);
+            this.btnConvert.TabIndex = 2;
+            this.btnConvert.Text = "轉換";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnClear.Location = new System.Drawing.Point(713, 43);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(84, 34);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "清除";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "MainForm";
+            this.Text = "Show RTF";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.RichTextBox txtInput;
+        private System.Windows.Forms.RichTextBox txtOutput;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ShowRTF
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            txtOutput.Text = "";
+            try
+            {
+                if (txtInput.Text.Trim() != "") txtOutput.Rtf = txtInput.Text.Trim();
+
+                Clipboard.Clear();
+                Clipboard.SetData(DataFormats.Rtf, txtInput.Text.Trim());
+                txtOutput.Text = Clipboard.GetData(DataFormats.Html).ToString();
+            }
+            catch { }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtInput.Text = "";
+            txtOutput.Text = "";
+
+        }
+
+        private void txtInput_DoubleClick(object sender, EventArgs e)
+        {
+            txtInput.Text = "";
+        }
+    }
+}
